b04b7535f4fdba1a7e714d0a54525576ea02f2d3
ruzen42 <malledeferlive@gmail.com>
2025-12-06 14:51:58 +0600

    feat(add working db)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
-import Lib
+import DB
+import Database.SQLite.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+  conn <- open "example.db"
+  initDB conn
+  addUser conn "Ruzen42"
+  addUser conn "Determine"
+  users <- getUsers conn
+  mapM_ print users
+  close conn
+
+
